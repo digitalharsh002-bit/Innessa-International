36d897a9c5e261ed46114ee59a4b6ee676909455
Kunal Chauhan <digitalrajput06@gmail.com>
2026-04-29 11:55:45 +0530

    final preview version

--- innessa/app/layout.tsx
+++ innessa/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../styles/globals.css";
 import { CartProvider } from "@/lib/CartContext";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
